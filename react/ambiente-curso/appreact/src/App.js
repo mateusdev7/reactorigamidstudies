7e6bcccb1c0f7af527dec33581c46277fd9c918f
mateusdev7 <mateusdev1337@gmail.com>
2022-11-02 14:45:16 -0300

    Jsx parte 1

--- react/ambiente-curso/appreact/src/App.js
+++ react/ambiente-curso/appreact/src/App.js
@@ -1,9 +1,48 @@
-const  App = () => {
+import React from "react"
+
+const titulo = <h1>Esse é um título</h1>
+
+const App = () => {
+  const nome = 'Mateus';
+  const ativo = true;
+
+  function mostrarNome(sobrenome ) {
+    return ` Julia ${sobrenome}`
+  }
+
+  const carro = {
+    marca: 'Mercedes',
+    piloto: 'Hamilton',
+    numero: 44,
+  }
+
+  const estilos = {
+    color: 'blue',
+    fontSize: '20px',
+    fontFamily: 'Arial'
+  }
+
   return (
-    <div >
-      Meu App
-    </div>
-  );
+    <React.Fragment>
+      <a className="ativo" href="https://origamid.com" title="Isso é um site">
+        Origamid
+      </a>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" />
+      {titulo}
+      <p>
+        {true ? 'Teste 1' : 'Teste 2'} -
+        {mostrarNome('Machado')}
+      </p>
+      <p style={estilos}>
+        {`${new Date().getDate()} / ${new Date().getMonth() + 1} / ${new Date().getFullYear()}`}
+      </p>
+      <p className={ativo ? "ativo" : "inativo"}>{nome}</p>
+      <p>
+        {carro.marca}
+      </p>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
