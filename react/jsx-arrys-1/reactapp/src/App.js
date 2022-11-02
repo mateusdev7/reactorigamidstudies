d220fe5a2253193303350eec3cb41e9e1a5f0970
mateusdev7 <mateusdev1337@gmail.com>
2022-11-02 16:05:13 -0300

    JSX Arrays 1

---
+++ react/jsx-arrys-1/reactapp/src/App.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+const App = () => {
+  const filmes = ["Before Sunrise", "Before Sunset", "Before Midnight"]
+
+  const livros = [
+    { nome: "A Game of Thrones", ano: 1996 },
+    { nome: "A Clash of Kings", ano: 1998 },
+    { nome: "A Storm of Swords", ano: 2000 },
+  ]
+
+  return (
+    <React.Fragment>
+      <ul>
+        {filmes.map((filme) => (
+          <li key={filme}>{filme}</li>
+        ))}
+      </ul>
+
+      <ul>
+        {livros.filter(({ano}) => ano >= 2000).map(({nome, ano}) => (
+          <li key={nome}>
+            {nome}, {ano}
+          </li>
+        ))}
+      </ul>
+
+      <ul>
+        {livros.filter(({ano}) => ano >= 1998).map(({ nome, ano }) => (
+          <li key={nome}>
+            {nome}, {ano}
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  )
+}
+
+export default App
