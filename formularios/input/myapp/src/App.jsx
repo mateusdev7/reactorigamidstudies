84291690d67b9a461f1c79464eb63a24e916fd5e
mateusdev7 <mateusdev1337@gmail.com>
2022-11-30 13:59:30 -0300

    Formulários - Input

---
+++ formularios/input/myapp/src/App.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+const formFields = [
+  {
+    id: "nome",
+    label: "Nome",
+    type: "text",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    id: "senha",
+    label: "Senha",
+    type: "password",
+  },
+  {
+    id: "cep",
+    label: "Cep",
+    type: "text",
+  },
+  {
+    id: "rua",
+    label: "Rua",
+    type: "text",
+  },
+  {
+    id: "numero",
+    label: "Número",
+    type: "text",
+  },
+  {
+    id: "bairro",
+    label: "Bairro",
+    type: "text",
+  },
+  {
+    id: "cidade",
+    label: "Cidade",
+    type: "text",
+  },
+  {
+    id: "estado",
+    label: "Estado",
+    type: "text",
+  },
+];
+
+function App() {
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: "",
+      };
+    }, {})
+  );
+
+  const [responseFetch, setResponseFetch] = React.useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://ranekapi.origamid.dev/json/api/usuario", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponseFetch(response);
+    });
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input
+            name={id}
+            id={id}
+            type={type}
+            value={form[id]}
+            onChange={handleChange}
+          />
+        </div>
+      ))}
+      {responseFetch && responseFetch.ok && responseFetch.status === 200 && (
+        <p>Formulário enviado</p>
+      )}
+      <button>Enviar</button>
+    </form>
+  );
+}
+
+export default App;
