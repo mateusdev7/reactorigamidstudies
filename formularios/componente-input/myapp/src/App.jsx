663401fe883e6a756ac4e8c75dc7ec035fb8a816
mateusdev7 <mateusdev1337@gmail.com>
2022-12-01 12:03:58 -0300

    Componentes - Radio

--- formularios/componente-input/myapp/src/App.jsx
+++ formularios/componente-input/myapp/src/App.jsx
@@ -1,18 +1,31 @@
 import Input from "./Form/Input.jsx";
 import Select from "./Form/Select.jsx";
 import React from "react";
+import Radio from "./Form/Radio.jsx";
 
 function App() {
-  const [nome, setNome] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [produto, setProduto] = React.useState("");
+  const [nome, setNome] = React.useState(""); // Input
+  const [email, setEmail] = React.useState(""); // Input
+  const [produto, setProduto] = React.useState(""); // Select
+  const [cor, setCor] = React.useState(""); // Radio
+  const [fruta, setFruta] = React.useState(""); // Radio
   return (
     <form action="">
+      <h2>Cores</h2>
+      <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
+      <h2>Frutas</h2>
+      <Radio
+        options={["Melancia", "Banana", "Uva", 'Laranja']}
+        value={fruta}
+        setValue={setFruta}
+      />
+      <h2>Produtos</h2>
       <Select
         options={["Smartphone", "Tablet"]}
         value={produto}
         setValue={setProduto}
       />
+      <h2>Preencha seus dados</h2>
       <Input
         id="nome"
         labelName="Nome"
