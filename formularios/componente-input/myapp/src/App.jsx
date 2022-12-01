dfef969dcbad8d89161c26b6791abfc41e83dd59
mateusdev7 <mateusdev1337@gmail.com>
2022-12-01 12:18:28 -0300

    Componentes - Checkbox

--- formularios/componente-input/myapp/src/App.jsx
+++ formularios/componente-input/myapp/src/App.jsx
@@ -2,6 +2,7 @@ import Input from "./Form/Input.jsx";
 import Select from "./Form/Select.jsx";
 import React from "react";
 import Radio from "./Form/Radio.jsx";
+import Checkbox from "./Form/Checkbox.jsx";
 
 function App() {
   const [nome, setNome] = React.useState(""); // Input
@@ -9,13 +10,28 @@ function App() {
   const [produto, setProduto] = React.useState(""); // Select
   const [cor, setCor] = React.useState(""); // Radio
   const [fruta, setFruta] = React.useState(""); // Radio
+  const [linguagens, setLinguagens] = React.useState([]); // Checkbox
+  const [termos, setTermos] = React.useState([]); // Checkbox
   return (
     <form action="">
+      <h2>Termos</h2>
+      <Checkbox
+        options={["Li e aceitos os termos."]}
+        value={termos}
+        setValue={setTermos}
+      />
+
+      <h2>Escolha sua Linguagem preferida</h2>
+      <Checkbox
+        options={["Javascript", "Java", "C", "C++", "Cobol", "Ada", "PHP"]}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
       <h2>Cores</h2>
       <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
       <h2>Frutas</h2>
       <Radio
-        options={["Melancia", "Banana", "Uva", 'Laranja']}
+        options={["Melancia", "Banana", "Uva", "Laranja"]}
         value={fruta}
         setValue={setFruta}
       />
