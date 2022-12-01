97e498cf5f9efc72a1d919f09597df4710267821
mateusdev7 <mateusdev1337@gmail.com>
2022-12-01 11:52:05 -0300

    Componentes - Select

--- formularios/componente-input/myapp/src/App.jsx
+++ formularios/componente-input/myapp/src/App.jsx
@@ -1,11 +1,18 @@
 import Input from "./Form/Input.jsx";
+import Select from "./Form/Select.jsx";
 import React from "react";
 
 function App() {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
+  const [produto, setProduto] = React.useState("");
   return (
     <form action="">
+      <Select
+        options={["Smartphone", "Tablet"]}
+        value={produto}
+        setValue={setProduto}
+      />
       <Input
         id="nome"
         labelName="Nome"
