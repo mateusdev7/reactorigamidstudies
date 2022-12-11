968521850d31768da67e7e6180b3006ff79aaf06
mateusdev7 <mateusdev1337@gmail.com>
2022-12-11 12:39:13 -0300

    Animações - Slide

---
+++ css/animacoes/my-app/src/App.jsx
@@ -0,0 +1,25 @@
+import Slide from './Slide.jsx'
+
+function App() {
+  const slides = [
+    {
+      id: "slide1",
+      text: "Slide 1",
+    },
+    {
+      id: "slide2",
+      text: "Slide 2",
+    },
+    {
+      id: "slide3",
+      text: "Slide 3",
+    },
+  ];
+  return (
+    <div>
+      <Slide slides={slides}/>
+    </div>
+  );
+}
+
+export default App;
